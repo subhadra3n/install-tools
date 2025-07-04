354fede4cf896af336fe1e6d19a9e1b6449a0469
subhadra3n <133737511+subhadra3n@users.noreply.github.com>
2025-07-04 13:32:49 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,14 +1,11 @@
 use t2.meduim only---t2.micro is not sufficient
 
-docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
-
-not working
 
 
+sudo docker run -d -p 9000:9000 sonarqube:lts-community
 
-docker run -d --name sonarqube \
-  -p 9000:9000 \
-  -v sonarqube_data:/opt/sonarqube/data \
-  -v sonarqube_extensions:/opt/sonarqube/extensions \
-  -v sonarqube_logs:/opt/sonarqube/logs \
-  sonarqube:lts
+
+
+
+
+
