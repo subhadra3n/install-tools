591b153f5b0128a62e273dc265fd05b9050e9a76
subhadra3n <133737511+subhadra3n@users.noreply.github.com>
2026-04-25 14:29:48 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -19,10 +19,11 @@ sudo apt upgrade -y
 
 sudo apt install openjdk-17-jre -y
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y 
-sudo apt-get install jenkins -y
+sudo apt update
+sudo apt install jenkins
